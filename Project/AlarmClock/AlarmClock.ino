6227d035ab8612008946530f120d117e88d2d920
JinWoo Ji <cream5343@gmail.com>
2022-03-06 20:10:35 +0900

    arduino

--- Project/AlarmClock/AlarmClock.ino
+++ Project/AlarmClock/AlarmClock.ino
@@ -1,28 +1,77 @@
 #include <LiquidCrystal.h>
+#include <core_build_options.h>
+#include <swRTC.h>
 
 LiquidCrystal lcd(2, 3, 4, 5, 6, 7, 8);
 
-int piezo = 9;
-int sw = 10;
+swRTC rtc;
 
-int tones[8] = {261, 294, 330, 349, 392, 440, 494, 523};
+int piezo = 9;    //알람 사운드
+int sw = 11;      //알람 종료 버튼
 
-int now;
+int alarmHour = 2;    //알람이 울리는 시
+int alarmMinute = 8;  //알람이 울리는 분
 
 void setup(){
+  rtc.stopRTC();  //정지
+  rtc.setTime(2, 7, 35); //시간, 분, 초 초기화
+  rtc.setDate(5, 3, 2022); //일, 월, 년 초기화 
+  rtc.startRTC(); //시작
+
+  lcd.begin(16, 2);
+  lcd.clear();
+
   pinMode(piezo, OUTPUT);
   pinMode(sw, INPUT_PULLUP);
-  lcd.begin(16, 2);
-  lcd.clear;
 }
 
 void loop(){
-  int tone[] = {};
-  if(digitalRead(sw) == LOW){
-    noTone(piezo);
+  //오늘 날짜와 현재 시간 LCD 화면 상에 출력
+  lcd.setCursor(0,0);
+  lcd.print("[");
+  numberPrint(rtc.getMonth());
+  lcd.print("/");
+  numberPrint(rtc.getDay());
+  lcd.print("]");
+  setAMPM(rtc.getHours());
+  numberPrint(rtc.getHours());
+  lcd.print(":");
+  numberPrint(rtc.getMinutes());
+
+  //설정된 알람 시간을 LCD 화면 상에 출력
+  lcd.setCursor(0,1);
+  lcd.print("Alarm");
+  lcd.setCursor(7,1);
+  setAMPM(alarmHour);
+  numberPrint(alarmHour);
+  lcd.print(":");
+  numberPrint(alarmMinute);
+
+  //지정된 시간에 알람이 울리고 스위치를 누르면 지정된 알람 시간 초기화
+  alarmClock(alarmHour, alarmMinute);
+  if(digitalRead(sw) == 0){
+    analogWrite(piezo, 0);
+    alarmHour = 0;
+    alarmMinute = 0;
   }
 }
 
-void set_time(){
-  
+
+void alarmClock(int checkHour, int checkMinute){      //알람이 울리는 시간이라면 알람 울리게 한다
+  if(checkHour == rtc.getHours() && checkMinute == rtc.getMinutes()){
+  analogWrite(piezo, 128);
+  }
+}
+
+void setAMPM(int hour){         //AM, PM을 설정해주는 함수
+  if(hour >= 12) lcd.print("PM");
+  else lcd.print("AM");
+}
+
+void numberPrint(int number){   //10보다 작은 수가 출력 될 때 앞에 0을 출력하고 아니면 원래의 숫자 출력
+  if(number < 10){
+    lcd.print("0");
+    lcd.print(number);
+  }
+  else lcd.print(number);
 }
--- Project/SmartRecycleBin/SmartRecycleBin.ino
+++ Project/SmartRecycleBin/SmartRecycleBin.ino
@@ -2,7 +2,7 @@
 
 Servo motor;
 
-int motorDegree = 0;  //서브모터의 위치 저장 변수
+int motorDegree = 0;  //서브모터 초기 위치 저장 변수
 
 int echo = 7;
 int trig = 8;
@@ -21,16 +21,18 @@ void loop(){
 
   float duration = pulseIn(echo, HIGH); //echo 핀이 HIGH를 유지한 시간. 즉, 초음파를 보냈다가 다시 돌아온 시간
 
-  float distance = duration / 58.8;
+  float distance = duration / 58.8;   //초음파 값 cm 단위로 변환
 
-  if(distance < 10){
-    for(int i = 0; i < 30; i++){
+  if(distance < 10){    //거리가 10cm 이하이면
+    for(int i = 0; i < 30; i++){    //휴지통 열기
       motorDegree += 2;
       motor.write(motorDegree);
       delay(10);
     }
-    delay(2000);
-    for(int i = 0; i < 30; i++){
+    
+    delay(2000);    //휴지통이 열린 상태에서 2초간 대기
+    
+    for(int i = 0; i < 30; i++){    //휴지통 닫기
       motorDegree -= 2;
       motor.write(motorDegree);
       delay(10);
